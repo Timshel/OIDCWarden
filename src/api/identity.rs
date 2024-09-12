nodomain/vaultwarden

Allow SSO role mapping to add admin cookie

src/api/identity.rs
@@ -9,6 +9,7 @@ use serde_json::Value;
 
 use crate::{
     api::{
+        admin,
         core::{
             accounts::{PreloginData, RegisterData, _prelogin, _register, kdf_upgrade},
             log_user_event,
@@ -29,7 +30,12 @@ pub fn routes() -> Vec<Route> {
 }
 
 #[post("/connect/token", data = "<data>")]
-async fn login(data: Form<ConnectData>, client_header: ClientHeaders, mut conn: DbConn) -> JsonResult {
+async fn login(
+    data: Form<ConnectData>,
+    client_header: ClientHeaders,
+    cookies: &CookieJar<'_>,
+    mut conn: DbConn,
+) -> JsonResult {
     let data: ConnectData = data.into_inner();
 
     let mut user_uuid: Option<String> = None;
@@ -49,7 +55,7 @@ async fn login(data: Form<ConnectData>, client_header: ClientHeaders, mut conn: 
             _check_is_some(&data.device_name, "device_name cannot be blank")?;
             _check_is_some(&data.device_type, "device_type cannot be blank")?;
 
-            _password_login(data, &mut user_uuid, &mut conn, &client_header.ip).await
+            _password_login(data, &mut user_uuid, &mut conn, cookies, &client_header.ip).await
         }
         "client_credentials" => {
             _check_is_some(&data.client_id, "client_id cannot be blank")?;
@@ -70,7 +76,7 @@ async fn login(data: Form<ConnectData>, client_header: ClientHeaders, mut conn: 
             _check_is_some(&data.device_name, "device_name cannot be blank")?;
             _check_is_some(&data.device_type, "device_type cannot be blank")?;
 
-            _authorization_login(data, &mut user_uuid, &mut conn, &client_header.ip).await
+            _authorization_login(data, &mut user_uuid, &mut conn, cookies, &client_header.ip).await
         }
         t => err!("Invalid type", t),
     };
@@ -158,6 +164,7 @@ async fn _authorization_login(
     data: ConnectData,
     user_uuid: &mut Option<String>,
     conn: &mut DbConn,
+    cookies: &CookieJar<'_>,
     ip: &ClientIp,
 ) -> JsonResult {
     let scope = match data.scope.as_ref() {
@@ -186,7 +193,7 @@ async fn _authorization_login(
         }
     };
 
-    let refresh_token = sso::redeem(code, conn).await?;
+    let au_user = sso::redeem(code, conn).await?;
 
     let now = Utc::now().naive_utc();
     let (user, mut device, new_device, twofactor_token) = match user_data {
@@ -202,16 +209,31 @@ async fn _authorization_login(
         }
     };
 
-    device.refresh_token = refresh_token; // will be saved in authenticated_response
+    let is_admin = au_user.is_admin();
+    device.refresh_token = au_user.refresh_token; // will be saved in authenticated_response
     *user_uuid = Some(user.uuid.clone()); // Set the user_uuid to be passed back used for event logging.
 
-    authenticated_response(scope, scope_vec, &user, &mut device, new_device, twofactor_token, &now, conn, ip).await
+    authenticated_response(
+        scope,
+        scope_vec,
+        &user,
+        is_admin,
+        &mut device,
+        new_device,
+        twofactor_token,
+        &now,
+        conn,
+        cookies,
+        ip,
+    )
+    .await
 }
 
 async fn _password_login(
     data: ConnectData,
     user_uuid: &mut Option<String>,
     conn: &mut DbConn,
+    cookies: &CookieJar<'_>,
     ip: &ClientIp,
 ) -> JsonResult {
     // Validate scope
@@ -321,7 +343,20 @@ async fn _password_login(
 
     let twofactor_token = twofactor_auth(&user, &data, &mut device, ip, conn).await?;
 
-    authenticated_response(scope, scope_vec, &user, &mut device, new_device, twofactor_token, &now, conn, ip).await
+    authenticated_response(
+        scope,
+        scope_vec,
+        &user,
+        false,
+        &mut device,
+        new_device,
+        twofactor_token,
+        &now,
+        conn,
+        cookies,
+        ip,
+    )
+    .await
 }
 
 #[allow(clippy::too_many_arguments)]
@@ -329,11 +364,13 @@ async fn authenticated_response(
     scope: &String,
     scope_vec: Vec<String>,
     user: &User,
+    is_admin: bool,
     device: &mut Device,
     new_device: bool,
     twofactor_token: Option<String>,
     now: &NaiveDateTime,
     conn: &mut DbConn,
+    cookies: &CookieJar<'_>,
     ip: &ClientIp,
 ) -> JsonResult {
     if CONFIG.mail_enabled() && new_device {
@@ -364,6 +401,11 @@ async fn authenticated_response(
                 }
             }
         }
+    }
+
+    if is_admin {
+        info!("User {} logged with admin cookie", user.email);
+        cookies.add(admin::create_admin_cookie());
     }
 
     // Common

Timshel/OIDCWarden

Co-authored-by: Fabian Fischer <nodomain@users.noreply.github.com>

src/api/identity.rs
@@ -2,7 +2,7 @@ use chrono::{NaiveDateTime, Utc};
 use num_traits::FromPrimitive;
 use rocket::{
     form::{Form, FromForm},
-    http::Status,
+    http::{CookieJar, Status},
     response::Redirect,
     serde::json::Json,
     Route,
@@ -11,6 +11,7 @@ use serde_json::Value;
 
 use crate::{
     api::{
+        admin,
         core::{
             accounts::{PreloginData, RegisterData, _prelogin, _register, kdf_upgrade},
             log_user_event,
@@ -31,7 +32,12 @@ pub fn routes() -> Vec<Route> {
 }
 
 #[post("/connect/token", data = "<data>")]
-async fn login(data: Form<ConnectData>, client_header: ClientHeaders, mut conn: DbConn) -> JsonResult {
+async fn login(
+    data: Form<ConnectData>,
+    client_header: ClientHeaders,
+    cookies: &CookieJar<'_>,
+    mut conn: DbConn,
+) -> JsonResult {
     let data: ConnectData = data.into_inner();
 
     let mut user_uuid: Option<String> = None;
@@ -73,7 +79,7 @@ async fn login(data: Form<ConnectData>, client_header: ClientHeaders, mut conn: 
             _check_is_some(&data.device_name, "device_name cannot be blank")?;
             _check_is_some(&data.device_type, "device_type cannot be blank")?;
 
-            _sso_login(data, &mut user_uuid, &mut conn, &client_header.ip).await
+            _sso_login(data, &mut user_uuid, &mut conn, cookies, &client_header.ip).await
         }
         "authorization_code" => err!("SSO sign-in is not available"),
         t => err!("Invalid type", t),
@@ -152,7 +158,13 @@ async fn _refresh_login(data: ConnectData, conn: &mut DbConn) -> JsonResult {
 }
 
 // After exchanging the code we need to check first if 2FA is needed before continuing
-async fn _sso_login(data: ConnectData, user_uuid: &mut Option<String>, conn: &mut DbConn, ip: &ClientIp) -> JsonResult {
+async fn _sso_login(
+    data: ConnectData,
+    user_uuid: &mut Option<String>,
+    conn: &mut DbConn,
+    cookies: &CookieJar<'_>,
+    ip: &ClientIp,
+) -> JsonResult {
     AuthMethod::Sso.check_scope(data.scope.as_ref())?;
 
     // Ratelimit the login
@@ -247,6 +259,11 @@ async fn _sso_login(data: ConnectData, user_uuid: &mut Option<String>, conn: &mu
 
     // Set the user_uuid here to be passed back used for event logging.
     *user_uuid = Some(user.uuid.clone());
+
+    if auth_user.is_admin() {
+        info!("User {} logged with admin cookie", user.email);
+        cookies.add(admin::create_admin_cookie());
+    }
 
     let auth_tokens = sso::create_auth_tokens(
         &device,