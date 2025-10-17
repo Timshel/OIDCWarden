f5ab7654c60e2718d0c04589540314cdfcbf4e75
Timshel <timshel@users.noreply.github.com>
2025-10-17 15:10:54 +0200

    Organization add dummy seats value

--- src/db/models/organization.rs
+++ src/db/models/organization.rs
@@ -190,7 +190,7 @@ impl Organization {
         json!({
             "id": self.uuid,
             "name": self.name,
-            "seats": null,
+            "seats": u32::MAX,
             "maxCollections": null,
             "maxStorageGb": i16::MAX, // The value doesn't matter, we don't check server-side
             "use2fa": true,
@@ -566,7 +566,7 @@ impl Membership {
             "id": self.org_uuid,
             "identifier": null, // Not supported
             "name": org.name,
-            "seats": null,
+            "seats": u32::MAX,
             "maxCollections": null,
             "usersGetPremium": true,
             "use2fa": true,
